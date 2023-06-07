6d191004a1627e905ff920c2880b2cccff4bba56
William Pennell <william.pennell@jumar.co.uk>
2023-06-07 16:19:09 +0100

    init setup for Welcome comp and Register comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { RegisterComponent } from './user-self-service/register/register.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
